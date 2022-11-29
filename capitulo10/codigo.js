87bf24693315962ca3063422f49c9c5753170a75
mariquenaallosa <allosamariquena@gmail.com>
2022-11-29 00:10:22 -0300

    Manejo de errores

--- capitulo10/codigo.js
+++ capitulo10/codigo.js
@@ -1,16 +1,41 @@
-let expr = "Banana"
-switch (expr){
+// let expr = "Banana"
+// switch (expr){
 
-case "Banana": 
-    console.log("esta fruta es amarilla");
-    break;
-case "Pera": 
-    console.log("tiene forma de mi tio Roberto");
-    break;
-case "Manzana": 
-    console.log("los primeris humanos pecanron comiendola");
-    break;
-default:
-    console.log("No es ninguna");
-    break;
+// case "Banana":
+//     console.log("esta fruta es amarilla");
+//     break;
+// case "Pera":
+//     console.log("tiene forma de mi tio Roberto");
+//     break;
+// case "Manzana":
+//     console.log("los primeris humanos pecanron comiendola");
+//     break;
+// default:
+//     console.log("No es ninguna");
+//     break;
+// }
+
+// const pruebaTry = ()=>{
+//     try {
+//         return 1;
+//     }
+//     catch(e){
+//         return 2;
+//     }
+//     finally {
+//         return 3;
+//     }
+
+// }
+// console.log(pruebaTry())
+
+try {
+  throw {
+    error: "error",
+    info: "info error"
+  };
+} catch (e) {
+  console.log(e);
+} finally {
+  
 }
