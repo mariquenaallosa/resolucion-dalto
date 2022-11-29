981b523ccee104b539dbd455323bfad315fba126
mariquenaallosa <allosamariquena@gmail.com>
2022-11-29 15:59:20 -0300

    trabajar callbacks

--- capitulo12/codigo.js
+++ capitulo12/codigo.js
@@ -3,8 +3,6 @@
 //     callback ("pedro")
 // }
 
-
-
 // function decirNombre(nombre){
 //     console.log(nombre);
 // }
@@ -25,5 +23,55 @@
 //     console.log(nombre);
 // })
 
-// simpleficado queda 
+// simpleficado queda
 // prueba(nombre=>console.log(nombre))
+
+class Persona {
+  constructor(nombre, instagram) {
+    this.nombre = nombre;
+    this.instagram = instagram;
+  }
+}
+
+const data = [
+  ["Mariquena Allosa", "@mariquenaallosa"],
+  ["Aldana Allosa", "@aldanaallosa"],
+  ["Rancio Ramirez", "@robrancio"],
+  ["Camila Nesa"],
+];
+
+const personas = [];
+for (var i = 0; i < data.length; i++) {
+  personas[i] = new Persona(data[i][0], data[i][1]);
+}
+
+const obtenerPersona = (id, cb) => {
+  if (personas[id] == undefined) {
+    cb("No se ha encontrado la persona");
+  } else {
+    cb(null, personas[id], id);
+  }
+};
+
+const obtenerInstagram = (id, cb) => {
+  if (personas[id] == undefined) {
+    cb("No se ha encontrado el instagram");
+  } else {
+    cb(null, personas[id].instagram);
+  }
+};
+
+obtenerPersona(2, (err, persona, id) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(persona.nombre);
+    obtenerInstagram(id, (err, instagram) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(instagram);
+      }
+    });
+  }
+});
