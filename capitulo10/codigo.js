98250eb2b32c9f23035bd5361626b850cec3076a
mariquenaallosa <allosamariquena@gmail.com>
2022-11-28 22:34:39 -0300

    switch

---
+++ capitulo10/codigo.js
@@ -0,0 +1,16 @@
+let expr = "Banana"
+switch (expr){
+
+case "Banana": 
+    console.log("esta fruta es amarilla");
+    break;
+case "Pera": 
+    console.log("tiene forma de mi tio Roberto");
+    break;
+case "Manzana": 
+    console.log("los primeris humanos pecanron comiendola");
+    break;
+default:
+    console.log("No es ninguna");
+    break;
+}
