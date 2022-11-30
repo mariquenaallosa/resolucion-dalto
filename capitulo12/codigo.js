c4d97022186c9d939b5d804726da3851e325581b
mariquenaallosa <allosamariquena@gmail.com>
2022-11-29 21:21:33 -0300

    promise

--- capitulo12/codigo.js
+++ capitulo12/codigo.js
@@ -26,52 +26,58 @@
 // simpleficado queda
 // prueba(nombre=>console.log(nombre))
 
-class Persona {
-  constructor(nombre, instagram) {
-    this.nombre = nombre;
-    this.instagram = instagram;
-  }
-}
 
-const data = [
-  ["Mariquena Allosa", "@mariquenaallosa"],
-  ["Aldana Allosa", "@aldanaallosa"],
-  ["Rancio Ramirez", "@robrancio"],
-  ["Camila Nesa"],
-];
 
-const personas = [];
-for (var i = 0; i < data.length; i++) {
-  personas[i] = new Persona(data[i][0], data[i][1]);
-}
+// **Ejemplo callbacks
+// class Persona {
+//   constructor(nombre, instagram) {
+//     this.nombre = nombre;
+//     this.instagram = instagram;
+//   }
+// }
 
-const obtenerPersona = (id, cb) => {
-  if (personas[id] == undefined) {
-    cb("No se ha encontrado la persona");
-  } else {
-    cb(null, personas[id], id);
-  }
-};
+// const data = [
+//   ["Mariquena Allosa", "@mariquenaallosa"],
+//   ["Aldana Allosa", "@aldanaallosa"],
+//   ["Rancio Ramirez", "@robrancio"],
+//   ["Camila Nesa"],
+// ];
 
-const obtenerInstagram = (id, cb) => {
-  if (personas[id] == undefined) {
-    cb("No se ha encontrado el instagram");
-  } else {
-    cb(null, personas[id].instagram);
-  }
-};
+// const personas = [];
+// for (var i = 0; i < data.length; i++) {
+//   personas[i] = new Persona(data[i][0], data[i][1]);
+// }
 
-obtenerPersona(2, (err, persona, id) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(persona.nombre);
-    obtenerInstagram(id, (err, instagram) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(instagram);
-      }
-    });
-  }
-});
+// const obtenerPersona = (id, cb) => {
+//   if (personas[id] == undefined) {
+//     cb("No se ha encontrado la persona");
+//   } else {
+//     cb(null, personas[id], id);
+//   }
+// };
+
+// const obtenerInstagram = (id, cb) => {
+//   if (personas[id] == undefined) {
+//     cb("No se ha encontrado el instagram");
+//   } else {
+//     cb(null, personas[id].instagram);
+//   }
+// };
+
+// obtenerPersona(2, (err, persona, id) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(persona.nombre);
+//     obtenerInstagram(id, (err, instagram) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log(instagram);
+//       }
+//     });
+//   }
+// });
+
+// ** Promesas 
+    
