b2df522d2caedc8cf4116f4f1541099bb49f393f
mariquenaallosa <allosamariquena@gmail.com>
2022-11-29 22:09:20 -0300

    Promesas

--- capitulo12/codigo.js
+++ capitulo12/codigo.js
@@ -26,8 +26,6 @@
 // simpleficado queda
 // prueba(nombre=>console.log(nombre))
 
-
-
 // **Ejemplo callbacks
 // class Persona {
 //   constructor(nombre, instagram) {
@@ -79,5 +77,77 @@
 //   }
 // });
 
-// ** Promesas 
-    
+// ** Promesas
+
+let nombre = "pedro";
+// Es un callback que dentro tiene dos callback
+// Aparecieron con ES6
+// presenta datos encapsulados
+const promesa = new Promise((resolve, reject) => {
+  if (nombre != "pedro") reject("Lo siento, el nombre no es pedro");
+  else resolve(nombre);
+});
+// console.log(promesa);
+
+// Para acceder a los valores
+// then() -> método que recibe un callback
+//        -> se usa para cuando sale todo bien y entramos a resolve
+// .catch() -> usamos para cuando entramos en reject
+promesa
+  .then((resultado) => {
+    console.log(resultado);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// **optimizar con promesas
+class Persona {
+  constructor(nombre, instagram) {
+    this.nombre = nombre;
+    this.instagram = instagram;
+  }
+}
+
+const data = [
+  ["Mariquena Allosa", "@mariquenaallosa"],
+  ["Aldana Allosa", "@aldanaallosa"],
+  ["Rancio Ramirez", "@robrancio"],
+  ["Camila Nesa"],
+];
+
+const personas = [];
+for (var i = 0; i < data.length; i++) {
+  personas[i] = new Persona(data[i][0], data[i][1]);
+}
+
+const obtenerPersona = (id) => {
+  return new Promise((res, rej) => {
+    if (personas[id] == undefined) rej("No se ha encontrado la persona");
+    else {
+      res(personas[id]);
+    }
+  });
+};
+
+const obtenerInstagram = (id) => {
+  return new Promise((res, rej) => {
+    if (personas[id].instagram == undefined)
+      rej("No se ha encontrado el instagram");
+    else {
+      res(personas[id].instagram);
+    }
+  });
+};
+
+let id = 2;
+obtenerPersona(id)
+  .then((persona) => {
+    console.log(persona.nombre);
+    return obtenerInstagram(id);
+  })
+  .then((instagram) => {
+    console.log(instagram);
+  }).catch((err) => {
+    console.log(err);
+  });
